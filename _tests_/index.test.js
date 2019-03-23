870d16a557f72c59a69ac80115c7546b33163167
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-23 13:55:39 +0530

    Add sample test

---
+++ _tests_/index.test.js
@@ -0,0 +1,9 @@
+import useAppState from '../src';
+
+describe('Test Template function', () => {
+  it('should be a function', () => {
+    expect(
+      typeof useAppState,
+    ).toBe('function');
+  });
+});
